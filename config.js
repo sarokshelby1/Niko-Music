3bbce263bb68acb8b1a1dde618c2179faa92de0b
sarok <70753730+sarokshelby1@users.noreply.github.com>
2020-12-30 02:26:30 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+exports.Token = "Lera tokin dane"; //Bot Token - Important
+exports.Owner = "Id xot"; //Bot Owner ID - Important
+exports.Default_Prefix = "?"; //Bot Default Prefix (Examples: A!, !)- Important
+exports.Color = "RANDOM"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
+exports.Support = `https://discord.gg/bFbr4cQRSh`; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
+
